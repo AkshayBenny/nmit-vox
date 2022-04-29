3a85d2dd1f6190d80e63cdaa7ed0ad2b78a7ac93
suvarneshkm <suvarnesh1729@gmail.com>
2022-04-29 17:26:36 +0530

    plugin

---
+++ plugin/code.js
@@ -0,0 +1,48 @@
+// This plugin will open a window to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.postMessage({ type: 'networkRequest' });
+figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
+    if (msg.replace(/^"|"$/g, '') == "create frame") {
+        const frame = figma.createFrame();
+        frame.resize(1080, 600);
+        figma.currentPage.appendChild(frame);
+    }
+    figma.closePlugin();
+});
+// figma.ui.onmessage = msg => {
+//   if (msg.type === 'create-rectangles') {
+//     const nodes: SceneNode[] = [];
+//     for (let i = 0; i < msg.count; i++) {
+//       const rect = figma.createRectangle();
+//       rect.x = i * 150;
+//       rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+//       figma.currentPage.appendChild(rect);
+//       nodes.push(rect);
+//     }
+//     figma.currentPage.selection = nodes;
+//     figma.viewport.scrollAndZoomIntoView(nodes);
+//   }
+//   if (msg.type === 'create-frame') {
+//     const frame = figma.createFrame();
+//     frame.resize(1080, 600);
+//     figma.currentPage.appendChild(frame);
+//   }
+//   figma.closePlugin();
+// };
